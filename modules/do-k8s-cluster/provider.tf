40352e83687c2e20d76ec4c98ebf52a0500bc3e3
Panachai <panachai.ny@gmail.com>
2021-08-23 01:06:26 +0700

    [provider-k8s] add verify script

--- modules/do-k8s-cluster/provider.tf
+++ modules/do-k8s-cluster/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "~> 2.4.0"
     }
 
     random = {
@@ -28,4 +28,12 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
   )
+
+  # verify
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "doctl"
+    args = ["kubernetes", "cluster", "kubeconfig", "exec-credential",
+    "--version=v1beta1", data.digitalocean_kubernetes_cluster.k8s.id]
+  }
 }
