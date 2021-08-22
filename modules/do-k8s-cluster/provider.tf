742756d6f379c3c909aada51fc256c06c2e82840
Panachai <panachai.ny@gmail.com>
2021-08-23 00:52:17 +0700

    format provider

--- modules/do-k8s-cluster/provider.tf
+++ modules/do-k8s-cluster/provider.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
 
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.1"
     }
 
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "~> 2.1"
     }
   }
@@ -22,8 +22,9 @@ provider "digitalocean" {
 }
 
 provider "kubernetes" {
-  host             = data.digitalocean_kubernetes_cluster.k8s.endpoint
-  token            = data.digitalocean_kubernetes_cluster.k8s.kube_config[0].token
+  host  = data.digitalocean_kubernetes_cluster.k8s.endpoint
+  token = data.digitalocean_kubernetes_cluster.k8s.kube_config[0].token
+
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
   )
