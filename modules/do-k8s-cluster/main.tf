19b9e760a2e625224e2247a3f5a051dd85e44659
Panachai <panachai.ny@gmail.com>
2021-08-23 01:07:33 +0700

    add VPC

--- modules/do-k8s-cluster/main.tf
+++ modules/do-k8s-cluster/main.tf
@@ -1,18 +1,23 @@
+data "digitalocean_kubernetes_versions" "k8s_versions" {
+  version_prefix = "1.21."
+}
+
 resource "random_pet" "node" {
   length    = 2
   separator = "-"
 }
 
-# resource "digitalocean_vpc" "example" {
-#   name     = "k8s-project-network"
-#   region   = var.region
-#   ip_range = "10.10.10.0/24"
-# }
+resource "digitalocean_vpc" "k8s-vpc" {
+  name   = "k8s-project-network"
+  region = var.region
+  # ip_range = "10.10.10.0/24"
+}
 
 resource "digitalocean_kubernetes_cluster" "k8s" {
-  name    = "k8s-${var.env}"
-  region  = var.region
-  version = data.digitalocean_kubernetes_versions.k8s_versions.latest_version
+  name     = "k8s-${var.env}"
+  region   = var.region
+  version  = data.digitalocean_kubernetes_versions.k8s_versions.latest_version
+  vpc_uuid = digitalocean_vpc.k8s-vpc.id
 
   node_pool {
     name = "${var.env}-${random_pet.node.id}-node-pool"
@@ -25,8 +30,4 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
     # min_nodes  = 1
     # max_nodes  = 5
   }
-}
-
-data "digitalocean_kubernetes_versions" "k8s_versions" {
-  version_prefix = "1.21."
 }
