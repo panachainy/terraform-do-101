7eac88ec74412547b8aee67ff5c00b5ff2f6cba1
Panachai <panachai.ny@gmail.com>
2021-08-23 00:37:13 +0700

    add require provider

--- modules/do-k8s-cluster/provider.tf
+++ modules/do-k8s-cluster/provider.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.1"
+    }
+
+    kubernetes ={
+      source = "hashicorp/kubernetes"
+      version = "~> 2.1"
+    }
+  }
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
